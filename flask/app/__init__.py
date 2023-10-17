0335c647cb74a711f1ae4387dfb97e805dce6397
Dhruv Mohapatra <dhrvmohapatra@gmail.com>
2023-10-18 00:14:20 +0530

    add quart for async handling

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from quart import Quart
 
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Quart(__name__)
 
 from . import views
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,13 +1,31 @@
-from flask import request
+import asyncio
+import multiprocessing
 
+# from flask import request, jsonify
+from quart import Quart, request, jsonify
 from . import app
 from .giffe import giffer
 
 
-@app.route('/giffe', methods=['GET'])
-def generate_gif():
+@app.route('/generate_gif')
+async def generate_gif():
+    print("url")
     url = request.args.get('url')
 
-    response = giffer(url)
+    # print("Create a multiprocessing Queue to retrieve the result")
+    # output_queue = multiprocessing.Queue()
+    # print(output_queue)
 
-    return response
+    # print("Start a separate process to run giffer")
+    # process = multiprocessing.Process(target=run_giffer, args=(url, output_queue))
+    # process.start()
+    # print("waiting for process to finish")
+    # process.join()  # Wait for the process to finish
+
+    print("Run the asynchronous giffer function")
+    result = await giffer(url)
+
+    # print("Get the result from the queue")
+    # result = output_queue.get()
+
+    return jsonify({'output_gif_path': result})
