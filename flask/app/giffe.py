40cde8833414929efc90162cfa7db58222bdf1c0
Dhruv Mohapatra <dhrvmohapatra@gmail.com>
2023-10-20 21:45:51 +0530

    fix scroll

--- flask/app/giffe.py
+++ flask/app/giffe.py
@@ -1,13 +1,13 @@
 import asyncio
 import os
 import uuid
-
+import random
 import boto3
 import cv2
 import numpy as np
 from botocore.exceptions import NoCredentialsError
 from moviepy.video.io.VideoFileClip import VideoFileClip
-from pyppeteer import launch
+from playwright.async_api import async_playwright
 
 from .config import Config
 
@@ -17,102 +17,117 @@ s3 = boto3.client('s3',
 
 
 async def giffer(url):
-    unique_id = str(uuid.uuid4())
-
-    browser = await launch(headless=True,
-                           ignoreHTTPSErrors=True,  # Ignore HTTPS-related errors
-                           args=[
-                               '--no-sandbox',
-                               '--disable-extensions',
-                               '--disable-dev-shm-usage',
-                               '--ignore-certificate-errors',  # Ignore SSL certificate errors
-                               '--disable-web-security',  # Disable web security, can help bypass CORS issues
-                               '--allow-running-insecure-content',  # Allow running insecure content
-                               '--disable-infobars'
-                           ])
-    page = await browser.newPage()
-
-    try:
-        # Set the user agent to mimic Google Chrome
-        user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-        await page.setUserAgent(userAgent=user_agent)
-        # Set the Accept-Language header to match the website's locale
-        await page.setExtraHTTPHeaders({'Accept-Language': 'en-US,en;q=0.9'})
-
-        width = 500
-        height = 500
-        await page.setViewport({'width': width, 'height': height})  # Set viewport to HD resolution (1920x1080)
-        await page.goto(url)
-
-        # Add another delay before continuing
-        await asyncio.sleep(3)
-
-        # Define the output video file path
-        output_video_path = f'outputs/{unique_id}.avi'
-
-        # define video codec etc.
-        fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        fps = 26
-        frame_delay = 5/fps
-
-        # Create a VideoWriter object to save the video
-        out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
-
-        # Get the initial height of the page
-        total_height = await page.evaluate('document.body.scrollHeight')
-
-        # Define the number of frames to scroll from top to middle and middle to bottom
-        num_frames = 20
-
-        # Scroll smoothly from top to middle
-        for i in range(num_frames):
-            scroll_position = i * total_height // num_frames
-            await page.addStyleTag({'content': 'body::-webkit-scrollbar { width: 0 !important; }'})
-            await page.evaluate(f'window.scrollTo(0, {scroll_position})')
-            await asyncio.sleep(frame_delay)  # Adjust the sleep time for the frame rate
-
-            # Capture a screenshot and write it to the video
-            screenshot = await page.screenshot({'type': 'jpeg'})
-            frame = np.frombuffer(screenshot, dtype=np.uint8)
-            frame = cv2.imdecode(frame, 1)
-            out.write(frame)
-
-        # Scroll smoothly from middle to bottom
-        for i in range(num_frames):
-            scroll_position = total_height - (i * total_height // num_frames)
-            await page.addStyleTag({'content': 'body::-webkit-scrollbar { width: 0 !important; }'})
-            await page.evaluate(f'window.scrollTo(0, {scroll_position})')
-            await asyncio.sleep(frame_delay)  # Adjust the sleep time for the frame rate
-
-            # Capture a screenshot and write it to the video
-            screenshot = await page.screenshot({'type': 'jpeg'})
-            frame = np.frombuffer(screenshot, dtype=np.uint8)
-            frame = cv2.imdecode(frame, 1)
-            out.write(frame)
-
-        # release VideoWriter
-        out.release()
-    finally:
-        await browser.close()
-
-    video_clip = VideoFileClip(output_video_path)
-    output_gif_path = f'outputs/{unique_id}.gif'
+    # viewports and stuff
+    width = 500
+    height = 500
+    scroll_down_pixels = 400
 
     if 'twitter.com' in url:
-        # Apply the crop_frame function to all frames in the video
-        cropped_clip = video_clip.fl_image(twitter_crop_frame)
+        scroll_down_pixels = 500
+    elif 'instagram.com' in url:
+        scroll_down_pixels = 600
 
-        # Write the cropped video as a GIF
-        cropped_clip.write_gif(output_gif_path, opt='wu')
-        cropped_clip.close()
-    else:
+    # Define the output video file path
+    unique_id = str(uuid.uuid4())
+    output_video_path = f'outputs/{unique_id}.avi'
+
+    # capture video and create gif
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True,
+                                          args=[
+                                              '--no-sandbox',
+                                              '--disable-extensions',
+                                              '--disable-dev-shm-usage',
+                                              '--ignore-certificate-errors',  # Ignore SSL certificate errors
+                                              '--disable-web-security',
+                                              # Disable web security, can help bypass CORS issues
+                                              '--allow-running-insecure-content',  # Allow running insecure content
+                                              '--disable-infobars'
+                                          ])
+
+        context = await browser.new_context(
+            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+            extra_http_headers={'Accept-Language': 'en-US,en;q=0.9'}
+        )
+        page = await context.new_page()
+
+        try:
+            await page.set_viewport_size({'width': width, 'height': height})
+            await page.goto(url)
+
+            # Add random delays between actions
+            await random_delay()
+
+            # define video codec etc.
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            fps = 30
+
+            # Create a VideoWriter object to save the video
+            out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+
+            # Define the number of frames to scroll down and then back up
+            num_frames = 30  # Adjust this number for the desired smoothness
+
+            # Define a flag to track if the close button was clicked
+            close_button_clicked = False
+
+            # Scroll smoothly from top to 400 pixels down
+            for i in range(num_frames):
+                if 'twitter.com' in url:
+                    await check_and_click_close_button_twitter(page, url)
+                elif 'instagram.com' in url:
+                    await check_and_click_close_button_insta(page, url)
+
+                scroll_position = (i * scroll_down_pixels) // num_frames
+                await page.add_style_tag(content='body::-webkit-scrollbar { width: 0 !important; }')
+                await page.evaluate(f'window.scrollTo(0, {scroll_position})')
+                # await asyncio.sleep(slow_frame_delay)  # Adjust the sleep time for the frame rate
+
+                # Capture a screenshot only if the close button was not clicked
+                if not close_button_clicked:
+                    screenshot = await page.screenshot(type='jpeg')
+                    frame = np.frombuffer(screenshot, dtype=np.uint8)
+                    frame = cv2.imdecode(frame, 1)
+                    out.write(frame)
+
+            # Reset the flag after scrolling down
+            close_button_clicked = False
+
+            # Scroll smoothly from 400 pixels back to the top
+            for i in range(num_frames):
+                if 'twitter.com' in url:
+                    await check_and_click_close_button_twitter(page, url)
+                elif 'instagram.com' in url:
+                    await check_and_click_close_button_insta(page, url)
+
+                scroll_position = scroll_down_pixels - (i * scroll_down_pixels) // num_frames
+                await page.add_style_tag(content='body::-webkit-scrollbar { width: 0 !important; }')
+                await page.evaluate(f'window.scrollTo(0, {scroll_position})')
+
+                # Capture a screenshot only if the close button was not clicked
+                if not close_button_clicked:
+                    screenshot = await page.screenshot(type='jpeg')
+                    frame = np.frombuffer(screenshot, dtype=np.uint8)
+                    frame = cv2.imdecode(frame, 1)
+                    out.write(frame)
+
+            # release VideoWriter
+            out.release()
+        finally:
+            await browser.close()
+
+        video_clip = VideoFileClip(output_video_path)
+        output_gif_path = f'outputs/{unique_id}.gif'
+
+        if 'twitter.com' in url:
+            video_clip = video_clip.fl_image(twitter_crop_frame)
+        elif 'instagram.com' in url:
+            video_clip = video_clip.fl_image(instagram_crop_frame)
+
         video_clip.write_gif(output_gif_path, opt='wu')
 
-    # Close the video clips
-    video_clip.close()
-
-    videoClip = VideoFileClip(output_video_path)
-    videoClip.write_gif(output_gif_path)
+        # Close the video clips
+        video_clip.close()
 
     # Upload the GIF to S3
     s3_key = f'gifs/{unique_id}.gif'  # Specify your desired S3 key
@@ -142,5 +157,39 @@ async def giffer(url):
 
 # Function to crop the frame by removing 75 pixels from left and bottom boundaries
 def twitter_crop_frame(frame):
-    # Crop 100 pixels from all boundaries
     return frame[:-75, 75:]
+
+
+def instagram_crop_frame(frame):
+    return frame[60:, :]
+
+
+# Define a function to check if the close button is present and click it
+async def check_and_click_close_button_twitter(page, url):
+    if await page.query_selector('[aria-label="Close"]'):
+        try:
+            # Find and click the element with aria-label "Close" if it exists
+            await page.click('[aria-label="Close"]')
+            # Set the flag to True when the close button is clicked
+            close_button_clicked = True
+        except Exception as e:
+            print(f"Error clicking the close button: {e}")
+
+
+# Define a function to check if the close button is present and click it
+async def check_and_click_close_button_insta(page, url):
+    if await page.query_selector('[aria-label="Close"]'):
+        try:
+            # Find and click the element with aria-label "Close" if it exists
+            await page.click('[aria-label="Close"]')
+            # Set the flag to True when the close button is clicked
+            close_button_clicked = True
+        except Exception as e:
+            print(f"Error clicking the close button: {e}")
+
+
+async def random_delay():
+    min_delay = 3  # Minimum delay in seconds
+    max_delay = 5  # Maximum delay in seconds
+    delay = random.uniform(min_delay, max_delay)
+    await asyncio.sleep(delay)
