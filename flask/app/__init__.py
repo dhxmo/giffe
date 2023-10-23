15c2b9191c550830ad7cadbd7670626e261f9f05
Dhruv Mohapatra <dhrvmohapatra@gmail.com>
2023-10-23 18:56:48 +0530

    ratelimit whoopsie?

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,9 +3,10 @@ from quart import Quart
 from .views import rate_limiter
 
 app = Quart(__name__, template_folder="templates")
-rate_limiter.init_app(app)
 
 from . import views
+
+rate_limiter.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- flask/app/views.py
+++ flask/app/views.py
@@ -7,8 +7,8 @@ from . import app
 from .giffe import giffer
 from .utils import sanitize_html
 
-
 rate_limiter = RateLimiter()
+
 
 @app.route('/')
 @rate_limit(1, timedelta(seconds=3))
@@ -17,7 +17,8 @@ async def index():
 
     content = await render_template('index.html')
     response = await make_response(content)
-    response.headers['Content-Security-Policy'] = "default-src 'self' https://giffe.s3.amazonaws.com/gifs/ https://giffe.s3.ap-south-1.amazonaws.com/share_buttons/ style-src 'self' 'unsafe-inline';"
+    response.headers[
+        'Content-Security-Policy'] = "default-src 'self' https://giffe.s3.amazonaws.com/gifs/ https://giffe.s3.ap-south-1.amazonaws.com/share_buttons/ style-src 'self' 'unsafe-inline';"
 
     return response
 
@@ -36,5 +37,3 @@ async def generate_gif():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory('static', 'assets/favicon.ico')
-
-
